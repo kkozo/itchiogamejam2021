553734f074f46336d82a53f230e1cfa16619ea07
Adam Pearson <adam.pearson94@gmail.com>
2021-11-02 14:14:32 +0000

    Current map fully loaded in game

--- src/scenes/game.ts
+++ src/scenes/game.ts
@@ -13,30 +13,37 @@ export class GameScene extends Phaser.Scene {
   }
 
   public preload(): void {
-    this.load.image('interiors_all', 'assets/tiles/interiors_32x32.png')
     this.load.image('interiors_kitchen', 'assets/tiles/kitchen_32x32.png')
     this.load.image('interiors_generic', 'assets/tiles/generic_32x32.png')
     this.load.image('interiors_bathroom', 'assets/tiles/bathroom_32x32.png')
     this.load.image('interiors_bedroom', 'assets/tiles/bedroom_32x32.png')
     this.load.image('interiors_living_room', 'assets/tiles/living_room_32x32.png')
     this.load.image('interiors_room_builder', 'assets/tiles/room_builder_32x32.png')
+    this.load.image('interiors_visible_upstairs', 'assets/tiles/visible_upstairs_32x32.png')
+    this.load.image('interiors_basement', 'assets/tiles/basement_32x32.png')
+    this.load.image('interiors_condo', 'assets/tiles/condo_32x32.png')
     this.load.tilemapTiledJSON('House01', 'assets/map//House01/House01.json')
 
   }
 
   public create(): void {
     const map = this.make.tilemap({key: 'House01'})
-    const allTiles = map.addTilesetImage('Interiors', 'interiors_all')
     const genericTiles = map.addTilesetImage('Generic', 'interiors_generic')
     const kitchenTiles = map.addTilesetImage('Kitchen', 'interiors_kitchen')
     const bathroomTiles = map.addTilesetImage('Bathroom', 'interiors_bathroom')
     const bedroomTiles = map.addTilesetImage('Bedroom', 'interiors_bedroom')
     const livingRoomTiles = map.addTilesetImage('Living Room', 'interiors_living_room')
+    const condoTiles = map.addTilesetImage('Condo', 'interiors_condo')
+    const basementTiles = map.addTilesetImage('Basement', 'interiors_basement')
+    const visUpstairsTiles = map.addTilesetImage('Visible Upstairs', 'interiors_visible_upstairs')
     const roomBuilderTiles = map.addTilesetImage('Room Builder', 'interiors_room_builder')
 
-    const groundLayer = map.createLayer('Floors', [allTiles, roomBuilderTiles])
-    const wallsLayer = map.createLayer('Walls', [allTiles, roomBuilderTiles])
-    const stat01Layer = map.createLayer('Static Objects 1', [allTiles, roomBuilderTiles, kitchenTiles, bedroomTiles, livingRoomTiles, genericTiles, bathroomTiles])
+    const groundLayer = map.createLayer('Floors', [roomBuilderTiles])
+    const wallsLayer = map.createLayer('Walls', [roomBuilderTiles])
+    const stat04Layer = map.createLayer('Static Objects 4', [roomBuilderTiles, kitchenTiles, bedroomTiles, livingRoomTiles, genericTiles, bathroomTiles])
+    const stat03Layer = map.createLayer('Static Objects 3', [roomBuilderTiles, kitchenTiles, bedroomTiles, livingRoomTiles, genericTiles, bathroomTiles])
+    const stat02Layer = map.createLayer('Static Objects 2', [roomBuilderTiles, kitchenTiles, bedroomTiles, livingRoomTiles, genericTiles, bathroomTiles])
+    const stat01Layer = map.createLayer('Static Objects 1', [roomBuilderTiles, condoTiles, kitchenTiles, bedroomTiles, livingRoomTiles, genericTiles, bathroomTiles])
   }
 
   public update(): void {
